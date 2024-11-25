f5e65edf613dc9e7f855127049e669afadd47822
rustamrv <rystamrv@gmail.com>
2024-11-25 20:28:17 +0200

    added

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+export function minimumTask() {
+  let firstName = "Rustam";
+  let lastName = "Ramazanov";
+  let age = 31;
+
+  return {
+    firstName,
+    lastName,
+    age,
+    isStudent: true,
+  };
+}
